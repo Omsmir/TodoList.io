0cf68578dfc5677fc65565c2374e36e13557e25b
Omar Samir <96446413+Omsmir@users.noreply.github.com>
2024-01-05 21:08:26 +0200

    Updates

--- js/main.js
+++ js/main.js
@@ -30,12 +30,18 @@ function empty(){
 
 submit.onclick = function (){
     if(text.value !== "" && provider.value !== ""&& label.value !== ""){
-        addElements(text.value,provider.value,label.value,colorPicker.value,colorLine.value)
+        let valid = /[a-z]/g;
+
+        let success = provider.value.match(valid).join("")
+        addElements(text.value,success,label.value,colorPicker.value,colorLine.value)
 
         empty()
 
     }else if ( text.value !== "" && provider.value !== "" ){
-            addElements(text.value,provider.value,"" ,colorPicker.value,colorLine.value)
+        let valid = /[a-z]/g;
+
+        let success = provider.value.match(valid).join("")
+            addElements(text.value,success,"" ,colorPicker.value,colorLine.value)
             empty()
     }else if (text.value !== ""){
         provider.focus()
